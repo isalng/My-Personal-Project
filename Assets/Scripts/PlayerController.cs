71d754459606273129bd435852e47189dd95335c
isalng <122053624+isalng@users.noreply.github.com>
2023-02-21 10:53:39 -0800

    Revert "Lab 3 Done"
    This reverts commit 927c44cd6edfb655ce5f5334817a310f19df487c.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,9 +6,6 @@ public class PlayerController : MonoBehaviour
 {
     private float speed = 10.0f;
     private Rigidbody playerRb;
-    private float zBound = 10;
-    private float xBound = 10;
-
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +13,7 @@ public class PlayerController : MonoBehaviour
         playerRb = GetComponent<Rigidbody>();
     }
 
-    // Movement of player
+    // Update is called once per frame
     void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
@@ -24,24 +21,5 @@ public class PlayerController : MonoBehaviour
 
         playerRb.AddForce(Vector3.forward * speed * verticalInput);
         playerRb.AddForce(Vector3.right * speed * horizontalInput);
-    // Pervent player go off scrren 
-        if (transform.position.z < -zBound)
-        {
-            transform.position = new Vector3(transform.position.x, transform.position.y, -zBound);
-        }       
-        if (transform.position.z > zBound)
-        {
-            transform.position = new Vector3(transform.position.x, transform.position.y, zBound);  
-        }
-
-
-        if (transform.position.x < -xBound)
-        {
-            transform.position = new Vector3(-xBound, transform.position.y, transform.position.z);
-        }       
-        if (transform.position.x >xBound)
-        {
-            transform.position = new Vector3(xBound, transform.position.y, transform.position.z);  
-        }
     }
 }
