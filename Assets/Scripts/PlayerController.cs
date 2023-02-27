0dc0346435fd46f6b8906f5353cfbe6c59b9aec3
isalng <122053624+isalng@users.noreply.github.com>
2023-02-27 11:52:46 -0800

    Fix Lab 3

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,9 @@ public class PlayerController : MonoBehaviour
 {
     private float speed = 10.0f;
     private Rigidbody playerRb;
+    private float zBound = 10;
+    private float xBound = 10;
+
 
     // Start is called before the first frame update
     void Start()
@@ -13,7 +16,7 @@ public class PlayerController : MonoBehaviour
         playerRb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
+    // Movement of player
     void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
@@ -21,5 +24,24 @@ public class PlayerController : MonoBehaviour
 
         playerRb.AddForce(Vector3.forward * speed * verticalInput);
         playerRb.AddForce(Vector3.right * speed * horizontalInput);
+    // Pervent player go off scrren 
+        if (transform.position.z < -zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zBound);
+        }       
+        if (transform.position.z > zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zBound);  
+        }
+
+
+        if (transform.position.x < -xBound)
+        {
+            transform.position = new Vector3(-xBound, transform.position.y, transform.position.z);
+        }       
+        if (transform.position.x >xBound)
+        {
+            transform.position = new Vector3(xBound, transform.position.y, transform.position.z);  
+        }
     }
 }
