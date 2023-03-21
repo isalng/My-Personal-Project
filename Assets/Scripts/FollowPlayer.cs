b1a7e732b315ce6fffe441cb2c53e18828f5a798
dav <usr21p1@gmail.com>
2023-03-21 10:13:18 -0700

    Added basic camera rotation

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -5,17 +5,32 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
+
+    private float speed = 3.0f;
+    private Vector3 lastMousePosition;
     private Vector3 offset = new Vector3(0, 5, -10);
     // Start is called before the first frame update
     void Start()
     {
-
+        lastMousePosition = Input.mousePosition;
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
         // Offset the camera behind the player by adding to the player's position
-        transform.position = player.transform.position + offset;
+        /* transform.position = player.transform.position + offset + new Vector3(Mathf.Cos(theta)); */
+
+        Vector3 relative = player.transform.position - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(relative, Vector3.up);
+        transform.rotation = rotation;
+
+        if(Input.GetMouseButton(0)) 
+        {
+            var mousePosition = Input.mousePosition;
+            var dtMousePosition = (lastMousePosition - mousePosition).normalized;
+            lastMousePosition = mousePosition;
+            transform.RotateAround(player.transform.position, Vector3.up, dtMousePosition.x * speed);
+        }
     }
 }
