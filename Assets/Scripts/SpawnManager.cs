d1b83bef93fbe34b0d92cd502d0fa6b5f3e3f414
isalng <122053624+isalng@users.noreply.github.com>
2023-04-03 11:40:51 -0700

    SpawnManager 1

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class SpawnManager : MonoBehaviour
 {
     public GameObject[] EnemyPrefabs;
     private float spawnRangeX = 15;
